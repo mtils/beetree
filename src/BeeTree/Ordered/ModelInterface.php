e32b17f89980efea716628759b4113f1c1688171
Michael Tils <mtils@web-utils.de>
2015-05-29 21:50:10 +0200

    Documentation and PSR-2 code style

--- src/BeeTree/Ordered/ModelInterface.php
+++ src/BeeTree/Ordered/ModelInterface.php
@@ -1,11 +1,30 @@
 <?php namespace BeeTree\Ordered;
 
-use BeeTree\ModelInterface as BaseModelInterface;
-use BeeTree\NodeInterface;
 
-interface ModelInterface extends BaseModelInterface{
-    public function insertAfter(NodeInterface $movedNode,
-                                NodeInterface $newAncestor);
-    public function insertBefore(NodeInterface $movedNode,
-                                 NodeInterface $newDescendant);
+use BeeTree\ModelInterface as BaseModelInterface;
+use BeeTree\NodeInterface as Node;
+
+interface ModelInterface extends BaseModelInterface
+{
+
+    /**
+     * Insert $moved after $newAncestor. The parents of both nodes are the same
+     * after this operation
+     *
+     * @param \BeeTree\NodeInterface $movedNode
+     * @param \BeeTree\NodeInterface $newAncestor
+     * @return self
+     **/
+    public function insertAfter(Node $movedNode, Node $newAncestor);
+
+    /**
+     * Insert $moved before $newAncestor. The parents of both nodes are the same
+     * after this operation.
+     *
+     * @param \BeeTree\NodeInterface $movedNode
+     * @param \BeeTree\NodeInterface $newAncestor
+     * @return self
+     **/
+    public function insertBefore(Node $movedNode, Node $newDescendant);
+
 }
